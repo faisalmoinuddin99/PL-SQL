384355accd4d8e6b3531ceaee83d807137728aa0
Mohd. Faisal Moinuddin Suleman <51179625+faisalmoinuddin99@users.noreply.github.com>
2021-03-31 12:23:20 +0530

    DataTypes in PL/SQL

---
+++ PL_SQL.sql
@@ -0,0 +1,19 @@
+-- Data Types
+
+/*
+
+1. Scalar : Scalar are of the following types: 
+            a. Numeric
+            b. Boolean
+            c. Character & String
+            d. Date & Time
+
+2. Composites: Composites are of the following types:
+                a. Record
+                b. Collection
+
+3. Large object (LOB): These are the pointers to the large objects that are stored separately.
+
+4. Reference : This is a pointer to other data item.
+                
+*/
