84aa48bc71d362fe643df1c961c62919ad49a511
Mohd. Faisal Moinuddin Suleman <51179625+faisalmoinuddin99@users.noreply.github.com>
2021-03-31 12:27:37 +0530

    Numeric Data Type

--- PL_SQL.sql
+++ PL_SQL.sql
@@ -15,5 +15,17 @@
 3. Large object (LOB): These are the pointers to the large objects that are stored separately.
 
 4. Reference : This is a pointer to other data item.
+
+
+****** Numeric Data Type ******
+
+# Numeric data type represents numbers, integers and floating-point numbers.
+
+# This type of data is stored as NUMBER (sql type) and PLS_INTEGER (specific to PL/SQL).
+
+# PLS_INTEGER is faster than NUMBER because it uses hardware arithmetic
+
+# PLS_INteGER data type is more space-optimised.
+
                 
 */
