96956255b81179a80c540388b242cdf20af37e77
Mohd. Faisal Moinuddin Suleman <51179625+faisalmoinuddin99@users.noreply.github.com>
2021-04-01 12:49:33 +0530

    IF-Then Syntax

--- PL_SQL.sql
+++ PL_SQL.sql
@@ -6,4 +6,32 @@ They have the following three forms:
 1. IF THEN
 2. IF THEN ELSE
 3. IF THEN ELSIF
+
+
+The syntax to be used for IF statements are as follows:
+
+1. IF THEN
+
+IF condition THEN
+ statements;
+END IF;
+
+2. IF THEN ELSE
+
+IF condition THEN
+ statements;
+ELSE
+ else_statements;
+END IF;
+
+3. IF THEN ELSIF
+
+IF condition_1 THEN
+ statements_1
+ELSIF condition_2 THEN
+ statements_2
+[ ELSE
+ else_statements
+]
+END IF;
 */
