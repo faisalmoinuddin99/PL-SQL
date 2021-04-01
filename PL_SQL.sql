9ffb89d9bb185f7ee14843f9c43f184c87a24e6f
Mohd. Faisal Moinuddin Suleman <51179625+faisalmoinuddin99@users.noreply.github.com>
2021-04-01 12:40:38 +0530

    IF statement Theory

--- PL_SQL.sql
+++ PL_SQL.sql
@@ -1,43 +1,9 @@
--- Variables in PL/SQL
+-- IF statements
 
 /*
-? These are named ‘storage locations’ and store values of a
-particular type.
-
-? Before using a variable, it has to be declared in the
-declaration section.
-
-The syntax used is as follows:
-variable_name datatype [NOT NULL] [:= expression];
-
-Notes:
-The local variable name should start with l_ and that of the global
-variable should start with g_.
+These are used to decide whether to allow or skip a sequence of statements.
+They have the following three forms:
+1. IF THEN
+2. IF THEN ELSE
+3. IF THEN ELSIF
 */
-
-DECLARE
-  
-  user_name VARCHAR2(20) DEFAULT 'Salman Khan';
-  last_name   VARCHAR2(20) := user_name ;
-BEGIN
-
-  user_name := 'Messi' ;
-  DBMS_OUTPUT.PUT_LINE(last_name); -- Salman Khan
-  DBMS_OUTPUT.PUT_LINE(user_name); --Messi
-  
-  last_name := 'Neymar';
-  DBMS_OUTPUT.PUT_LINE(last_name);
-  
-END ;
-
-Declare
-
-  a PLS_Integer := 10;
-  b PLS_Integer  := 20;
-  c PLS_Integer ;
-  
-Begin
-  c := a * b;
-  DBMS_OUTPUT.PUT_Line('Multiplcation of a * b is :' || c);
-  
-END ;
