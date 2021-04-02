5f06c4641b03096ba0b633c25d51fadce4e71700
Mohd. Faisal Moinuddin Suleman <51179625+faisalmoinuddin99@users.noreply.github.com>
2021-04-02 11:41:59 +0530

    Example 2

--- PL_SQL.sql
+++ PL_SQL.sql
@@ -12,3 +12,15 @@ BEGIN
     DBMS_OUTPUT.PUT_LINE('Odd Number') ;
   END IF ;
 END ;
+
+
+Declare
+a pls_integer := 10;
+b pls_integer := 30;
+c pls_integer := 20;
+begin
+if (a > b) then
+dbms_output.put_line('value of a is :' || a);
+end if;
+dbms_output.put_line('value of b is :' || b);
+end ;
