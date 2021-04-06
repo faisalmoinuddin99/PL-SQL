109c429bcac304e85a7fb76d33378006f9110078
Mohd. Faisal Moinuddin Suleman <51179625+faisalmoinuddin99@users.noreply.github.com>
2021-04-06 11:16:25 +0530

    Print all the number which are divisible by 2 and 3 between 1 nd 100

--- PL_SQL.sql
+++ PL_SQL.sql
@@ -1,54 +1,31 @@
--- Multiplication table of 5
-
-SET SERVEROUTPUT ON
+-- Print all the number which are divisible by 2 and 3 between 1 nd 100
 
 Declare 
-
-  table_of_five number(2) := 5 ;
-
-Begin
+  a number(3) ;
   
-  FOR a in 1 .. 10 LOOP
-    DBMS_OUTPUT.PUT_LINE('5 * ' ||a || ' = '  || table_of_five * a ) ;
-    
+BEGIN 
+  FOR a in 1 .. 100 LOOP
+    if MOD(a,2) = 0 AND MOD(a,3) = 0 THEN
+    dbms_output.put_line('Divisible by 2 and 3: ' || a) ;
+    END IF ;
   END LOOP ;
 END ;
 
 /*
-5 * 1 = 5
-5 * 2 = 10
-5 * 3 = 15
-5 * 4 = 20
-5 * 5 = 25
-5 * 6 = 30
-5 * 7 = 35
-5 * 8 = 40
-5 * 9 = 45
-5 * 10 = 50
-*/
-
-
--- Reverse For Loop
-
-DECLARE 
-  a number(2) ;
-
-BEGIN
-  FOR a IN Reverse 10 .. 20 LOOP
-    dbms_output.put_line(a) ;
-  END LOOP ;
-END ;
-
-/*
-20
-19
-18
-17
-16
-15
-14
-13
-12
-11
-10
+Divisible by 2 and 3: 6
+Divisible by 2 and 3: 12
+Divisible by 2 and 3: 18
+Divisible by 2 and 3: 24
+Divisible by 2 and 3: 30
+Divisible by 2 and 3: 36
+Divisible by 2 and 3: 42
+Divisible by 2 and 3: 48
+Divisible by 2 and 3: 54
+Divisible by 2 and 3: 60
+Divisible by 2 and 3: 66
+Divisible by 2 and 3: 72
+Divisible by 2 and 3: 78
+Divisible by 2 and 3: 84
+Divisible by 2 and 3: 90
+Divisible by 2 and 3: 96
 */
